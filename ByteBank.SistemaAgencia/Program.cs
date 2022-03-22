9b1a270fce1dc2dab5bc092cb2f96bb480d3c44f
Gabriel Sasaki <gabrielpintosasaki@outlook.com>
2022-03-22 03:07:29 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2022, 3, 20);
+
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(40);
+                                 //dataCorrente - dataFimPagamento;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.ReadLine();
+        }
+    }
+}
